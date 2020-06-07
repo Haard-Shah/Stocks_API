83dedebca28834204368ab822cae05f545f366c1
CAB230 <cab230@VDI-VL24B-118.qut.edu.au>
2020-06-07 16:47:47 +1000

    Stage 1, GET routes implemented - authed stocks route is still incomplete.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Stocks API
+
+Stocks API created using express and nodeJs. The app serves the webcomputing schema via its endpoints.
--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const knex_options = require('./knexfile');
+const knex = require('knex')(knex_options); // setup databse connection options
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,20 +17,29 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// ---- Middleware -----
+// db conneciton
+app.use((req, res, next) => {
+  req.db = knex;
+  next();
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
