0a278c86266f2d9230bcbdebc1d11fced4ecba5f
CAB230 <cab230@VDI-VL24B-118.qut.edu.au>
2020-06-07 17:31:16 +1000

    Added Swagger Docs on home route "/" - swagger docs execute feature is still not working

--- app.js
+++ app.js
@@ -1,37 +1,52 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const knex_options = require('./knexfile');
-const knex = require('knex')(knex_options); // setup databse connection options
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const knex_options = require("./knexfile");
+const knex = require("knex")(knex_options); // setup databse connection options
+const swaggerUI = require("swagger-ui-express");
+const yaml = require("yamljs");
+const swaggerDocumentation = yaml.load("./docs/swagger.yaml");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var stocksRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+logger.token("req", (req, res) => JSON.stringify(req.headers));
+logger.token("res", (req, res) => {
+  const headers = {};
+
+  res.getHeaderNames().map((key) => (headers[key] = res.getHeader(key)));
+
+  return JSON.stringify(headers);
+});
+app.use(logger("common"));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // ---- Middleware -----
 // db conneciton
 app.use((req, res, next) => {
   req.db = knex;
   next();
-})
+});
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/stocks", stocksRouter);
+app.use("/users", usersRouter);
+
+app.use("/", swaggerUI.serve, swaggerUI.setup(swaggerDocumentation));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -42,11 +57,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,42 +4,43 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", {
-    title: "Express"
+    title: "Express",
   });
 });
 
 /* Stocks Page */
-router.get("/stocks/symbols", (req, res, next) => {
-
+router.get("/symbols", (req, res, next) => {
   // Check if the req has a industry attached
   if (req.body.industry !== undefined) {
-    console.log(req.body.industry) // TODO: remove in production
-    console.log(/^[a-z]+$/i.test(req.body.industry))
+    console.log(req.body.industry); // TODO: remove in production
+    console.log(/^[a-z]+$/i.test(req.body.industry)); // TODO: remove in production
 
     if (!/^[a-z]+$/i.test(req.body.industry)) {
       res.status(400).json({
-        "error": true,
-        "message": "Invalid query parameter: only 'industry' is permitted"
-      })
+        error: true,
+        message: "Invalid query parameter: only 'industry' is permitted",
+      });
     } else {
       req.db
-        .from('stocks')
-        .distinct('name', 'symbol', 'industry')
-        .where('industry', 'LIKE', `%${req.body.industry}%`)
+        .from("stocks")
+        .distinct("name", "symbol", "industry")
+        .where("industry", "LIKE", `%${req.body.industry}%`)
         .then((rows) => {
-          console.log(rows.length) //TODO: Remove in production
+          console.log(rows.length); //TODO: Remove in production
 
           if (rows.length === 0) {
             res.status(404).json({
-              "error": true,
-              "message": "Industry sector not found"
-            })
+              error: true,
+              message: "Industry sector not found",
+            });
           }
 
-          res.json({ //TODO: Need to updated the response to have rows as an array of objects. 
-            rows
+          res.json({
+            //TODO: Need to updated the response to have rows as an array of objects.
+            rows,
           });
-        }).catch((err) => {
+        })
+        .catch((err) => {
           res.status(500).json({
             Error: true,
             Message: "Server Error - Error executing MySQL query",
@@ -47,12 +48,17 @@ router.get("/stocks/symbols", (req, res, next) => {
         });
     }
   } else {
-    req.db.from('stocks').distinct('name', 'symbol', 'industry')
+    req.db
+      .from("stocks")
+      .distinct("name", "symbol", "industry")
       .then((rows) => {
-        res.json({ //TODO: Need to updated the response to have rows as an array of objects instead of returning a object of array, where array is an array of objects. 
-          rows
+        console.log(rows.length); // TODO: remove in production
+        res.json({
+          //TODO: Need to updated the response to have rows as an array of objects instead of returning a object of array, where array is an array of objects.
+          rows,
         });
-      }).catch((err) => {
+      })
+      .catch((err) => {
         res.status(500).json({
           Error: true,
           Message: "Server Error - Error executing MySQL query",
@@ -62,23 +68,28 @@ router.get("/stocks/symbols", (req, res, next) => {
 });
 
 /* Unathenticated Specific Symbol Stock page */
-router.get("/stocks/:symbol", (req, res, next) => {
-
+router.get("/:symbol", (req, res, next) => {
   // Check if 'from' and 'to' were specified
   if (req.body.from || req.body.to) {
-    console.log('to and from were give for unathorised routes')
+    console.log("to and from were give for unathorised routes");
     res.status(400).json({
-      "error": true,
-      "message": "Date parameters only available on authenticated route /stocks/authed"
+      error: true,
+      message:
+        "Date parameters only available on authenticated route /stocks/authed",
     });
-  } else { // Simple Unauthed Specific stock query
-    req.db.from('stocks').select('*').where('symbol', '=', req.params.symbol)
+  } else {
+    // Simple Unauthed Specific symbol stock query
+    req.db
+      .from("stocks")
+      .select("*")
+      .where("symbol", "=", req.params.symbol)
       .then((rows) => {
-        if (rows.length === 0) { // unrecorded Symbol
+        if (rows.length === 0) {
+          // unrecorded Symbol
           res.status(404).json({
-            "error": true,
-            "message": "No entry for symbol in stocks database"
-          })
+            error: true,
+            message: "No entry for symbol in stocks database",
+          });
         } else {
           res.status(200).json(rows[0]);
         }
@@ -92,38 +103,47 @@ router.get("/stocks/:symbol", (req, res, next) => {
   }
 });
 
-/** 
+/**
  * Checks the validity of the user token.
  * @summary If the description is long, write your summary here. Otherwise, feel free to remove this.
  * @param {object} token - JWT token object to autherise the request.
  * @return {boolean} Return if the token is valid, if NOT handles the excpetions as well.
  */
 const tokenIsValid = (token) => {
-  console.log(token)
+  console.log(token); // TODO: Remove in production
   // Check token is valid and not expired
+  // FIXME: Add token validation logic.
   return true;
-}
+};
 
 /* Authenticated Specific Symbols Page */
-router.get("/stocks/authed/:symbol", (req, res, next) => {
-  // No token provided
+router.get("/authed/:symbol", (req, res, next) => {
   if (!req.body.token) {
+    // No token provided
     res.status(403).json({
-      "error": true,
-      "message": "Authorization header not found"
-    })
+      error: true,
+      message: "Authorization header not found",
+    });
   } else {
     if (tokenIsValid(req.body.token)) {
-
-      // TODO: check if there are from and to dates supplied
       if (req.body.from || req.body.to) {
-        console.log('from and to defeind');
-        console.log(req.body.from);
+        // check if there are from and to dates supplied
+        console.log("from and to defeind"); // TODO: Remove in production
+        console.log(req.body.from); // TODO: Remove in production
         req.db
-          .from('stocks')
-          .select('*')
-          .where('symbol', '=', req.params.symbol, 'AND', 'timestamp', '>=', req.body.from) // FIXME: The date comparison is broken the date seem to not be in the right format
+          .from("stocks")
+          .select("*")
+          .where(
+            "symbol",
+            "=",
+            req.params.symbol,
+            "AND",
+            "timestamp",
+            ">=",
+            req.body.from
+          ) // FIXME: The date comparison is broken the date seem to not be in the right format
           .then((row) => {
+            console.log(`${rows.length} Rows retertived`);
             res.json(rows);
           })
           .catch((err) => {
@@ -133,13 +153,16 @@ router.get("/stocks/authed/:symbol", (req, res, next) => {
             });
           });
       } else {
-        req.db.from('stocks').select('*').where('symbol', '=', req.params.symbol)
+        req.db
+          .from("stocks")
+          .select("*")
+          .where("symbol", "=", req.params.symbol)
           .then((rows) => {
             if (rows.length === 0) {
               res.status(404).json({
-                "error": true,
-                "message": "No entry for symbol in stocks database"
-              })
+                error: true,
+                message: "No entry for symbol in stocks database",
+              });
             } else {
               res.status(200).json(rows);
             }
@@ -153,16 +176,15 @@ router.get("/stocks/authed/:symbol", (req, res, next) => {
       }
     }
   }
-})
+});
 
 module.exports = router;
 
 // TODO: ASK
 /*
- * - if the date is beyond the start and end date of the database do we limit the dates to actual dates or do we return a error? 
+ * - if the date is beyond the start and end date of the database do we limit the dates to actual dates or do we return a error?
  * - can user only supply a start date and not an end date? and vice vera?
  * - Is it okay to have a long index.js file? are we expect to break our route handeling into smaller functions
- * - Does server need to support regex style industry querying where the query match a part of the search term?
  * - How to response with an array of objects? currently res.json returns an object of array with array being array of objects.
  * - Is there a smater way of formulating the queries especially for the where clause?
  * - Should we sort data before sending back to the user?
